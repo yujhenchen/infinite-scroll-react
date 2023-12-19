217780973b7bcc82f1754b63b87e72874efb5e25
yujhenchen <yujhen.chen93@gmail.com>
2023-12-19 20:31:47 +0800

    implement scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,61 @@
+import { useState, useEffect } from "react";
+import { Info } from "./types";
+
 function App() {
-  return <>Home</>;
+  const [items, setItems] = useState<Info[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  // const [error, setError] = useState(null);
+  const [page, setPage] = useState<number>(1);
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    // setError(null);
+
+    try {
+      const response = await fetch(`https://catfact.ninja/facts?page=${page}`);
+      const data = await response.json();
+
+      setItems((prevItems) => [...prevItems, ...data.data]);
+      setPage((prevPage) => prevPage + 1);
+    } catch (error) {
+      // setError(error);
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop !==
+        document.documentElement.offsetHeight ||
+      isLoading
+    ) {
+      return;
+    }
+    fetchData();
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [isLoading]);
+
+  return (
+    <div>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{item.fact}</li>
+        ))}
+      </ul>
+      {isLoading && <p>Loading...</p>}
+      {/* {error && <p>Error {error.message}</p>} */}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/types.ts
@@ -0,0 +1,4 @@
+export type Info = {
+    fact: string;
+    length: number;
+};
